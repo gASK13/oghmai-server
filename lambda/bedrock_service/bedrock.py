08b1d24e0002af6e9758c9781dec5322089d0d24
gASK13 <gask13@gmail.com>
2025-04-10 09:39:48 +0200

    Prompt format for my model.

--- lambda/bedrock_service/bedrock.py
+++ lambda/bedrock_service/bedrock.py
@@ -9,9 +9,11 @@ MAX_RETRIES = 3
 
 bedrock = boto3.client("bedrock-runtime", region_name="us-east-1")
 
+
 def load_prompt_template(name: str) -> str:
     with open(f"resources/prompts/{name}.txt", "r", encoding="utf-8") as f:
         return f.read()
+
 
 def bedrock_describe_word(definition: str) -> WordResult:
     prompt = load_prompt_template("describe_word").format(definition=definition)
@@ -29,20 +31,32 @@ def bedrock_describe_word(definition: str) -> WordResult:
             if attempt == MAX_RETRIES:
                 raise RuntimeError("Failed to get a valid response from Bedrock after multiple attempts.")
 
+
 def call_bedrock(prompt: str, temperature=0.7, max_tokens=500):
     response = bedrock.invoke_model(
         modelId=BEDROCK_MODEL_ID,
         body=bytes(
             json.dumps({
-                "prompt": prompt,
-                "temperature": temperature,
-                "max_tokens_to_sample": max_tokens,
-                "stop_sequences": ["\n\n"]
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [
+                            {"text": prompt}
+                        ]
+                    }
+                ],
+                "inferenceConfig": {
+                    "maxTokens": max_tokens,
+                    "stopSequences": [],
+                    "temperature": temperature,
+                    "topP": 0.9
+                }
             }),
             "utf-8"
         ),
         contentType="application/json",
         accept="application/json",
+        performanceConfig={"latency": "standard"}
     )
     response_body = response["body"].read().decode("utf-8")
     return json.loads(response_body)
